4a8302daf6fc941a0d02779c815fc6df3202f749
kumarpint00 <106054394+kumarpint0@users.noreply.github.com>
2024-06-12 16:09:51 +0530

    disable fetch call due to upgrade issue

--- portal/functions/index.js
+++ portal/functions/index.js
@@ -15,22 +15,22 @@ exports.nextjs = functions.https.onRequest((req, res) => {
 admin.initializeApp();
 const db = admin.firestore();
 
-exports.refreshMatchData = functions.pubsub.schedule('every 15 minutes').onRun(async (context) => {
-  try {
-    const response = await axios.get('https://api.cricapi.com/v1/cricScore?apikey=YOUR_API_KEY');
-    const data = response.data.data;
+// exports.refreshMatchData = functions.pubsub.schedule('every 15 minutes').onRun(async (context) => {
+//   try {
+//     const response = await axios.get('https://api.cricapi.com/v1/cricScore?apikey=YOUR_API_KEY');
+//     const data = response.data.data;
 
-    const batch = db.batch();
-    const matchDataRef = db.collection('matchData');
+//     const batch = db.batch();
+//     const matchDataRef = db.collection('matchData');
 
-    data.forEach((match) => {
-      const docRef = matchDataRef.doc(match.id.toString());
-      batch.set(docRef, match);
-    });
+//     data.forEach((match) => {
+//       const docRef = matchDataRef.doc(match.id.toString());
+//       batch.set(docRef, match);
+//     });
 
-    await batch.commit();
-    console.log('Match data refreshed successfully');
-  } catch (error) {
-    console.error('Error refreshing match data:', error);
-  }
-});
+//     await batch.commit();
+//     console.log('Match data refreshed successfully');
+//   } catch (error) {
+//     console.error('Error refreshing match data:', error);
+//   }
+// });
