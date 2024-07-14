4ed80a7a09c141eec18615930d506ba79e6e6e6a
Suresh Hastimal Jat <sjat112200@gmail.com>
2024-07-14 21:57:56 +0530

    Whatsapp on Landing Page.

--- portal/components/Pages/LandingPage/Home.tsx
+++ portal/components/Pages/LandingPage/Home.tsx
@@ -1,13 +1,15 @@
-"use client"
+"use client";
 import React, { useRef } from 'react';
 import Navbar from './Navbar';
 // import MarqueeText from './MarqueeText';
-import Slider from './GamesSlider'
-import ResultGamesColorComponent from './ResultPageGamesColorChange'
-import LandingPageImagesMarquee from './ImagesMarquee'
-import MainBodyScroller from './MainBodyScroller'
+import Slider from './GamesSlider';
+import ResultGamesColorComponent from './ResultPageGamesColorChange';
+import LandingPageImagesMarquee from './ImagesMarquee';
+import MainBodyScroller from './MainBodyScroller';
 import { useRouter } from 'next/navigation';
 import GamesCards from './GameCards';
+import { IconBrandWhatsapp } from '@tabler/icons-react';
+import { FaWhatsapp } from 'react-icons/fa';
 
 const Home: React.FC = () => {
     const router = useRouter();
@@ -17,7 +19,13 @@ const Home: React.FC = () => {
         gamesRef.current?.scrollIntoView({ behavior: 'smooth' });
     };
 
-    // const handleHelpClick = () => { router.push('/'); };
+    const handleWhatsAppClick = () => {
+        const adminPhoneNumber = '+918080917565';
+        const message = `Hello, I want to create an account. Please can you help me?`;
+        const whatsappURL = `https://wa.me/${adminPhoneNumber}?text=${encodeURIComponent(message)}`;
+        window.open(whatsappURL, '_blank');
+    }
+
 
     return (
         <div className=''>
@@ -31,10 +39,26 @@ const Home: React.FC = () => {
             </div>
             <ResultGamesColorComponent />
             <div className='w-full bg-white h-full'>
+                {/* <button
+                    onClick={handleWhatsAppClick()}
+                    className="whatsapp_float"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <IconBrandWhatsapp size={38} />
+                </button> */}
+                <button type="button" className="whatsapp_float" onClick={handleWhatsAppClick}>
+                    <IconBrandWhatsapp size={38} />
+
+                </button>
                 <hr className='border-t-2 border-gray-200' />
             </div>
+
+
+
+
         </div>
     );
-}
+};
 
 export default Home;
