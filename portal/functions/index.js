ee9bab40210f350d46be421d8971db558667fdc5
Suresh Hastimal Jat <sjat112200@gmail.com>
2024-08-05 09:21:54 +0530

    .

--- portal/functions/index.js
+++ portal/functions/index.js
@@ -5,31 +5,31 @@ const db = admin.firestore();
 
 // Settle Single Digit Lottery Bets every 120 seconds
 exports.settleSingleDigitLotteryBets = functions.pubsub
-  .schedule("every 15 minutes")
-  .onRun(async (context) => {
-    await settleLotteryBets("Single Digit Lottery", 1, 9);
-  });
+    .schedule("every 15 minutes")
+    .onRun(async (context) => {
+      await settleLotteryBets("Single Digit Lottery", 1, 9);
+    });
 
 // Settle Double Digit Lottery Bets every 300 seconds (5 minutes)
 exports.settleDoubleDigitLotteryBets = functions.pubsub
-  .schedule("every 15 minutes")
-  .onRun(async (context) => {
-    await settleLotteryBets("Double Digit Lottery", 10, 99);
-  });
+    .schedule("every 15 minutes")
+    .onRun(async (context) => {
+      await settleLotteryBets("Double Digit Lottery", 10, 99);
+    });
 
 // Settle Triple Digit Lottery Bets every 900 seconds (15 minutes)
 exports.settleTripleDigitLotteryBets = functions.pubsub
-  .schedule("every 15 minutes")
-  .onRun(async (context) => {
-    await settleLotteryBets("Triple Digit Lottery", 100, 999);
-  });
+    .schedule("every 15 minutes")
+    .onRun(async (context) => {
+      await settleLotteryBets("Triple Digit Lottery", 100, 999);
+    });
 
 // Settle Color Ball Game Bets every 900 seconds (15 minutes)
 exports.settleColorBallBets = functions.pubsub
-  .schedule("every 15 minutes")
-  .onRun(async (context) => {
-    await settleColorBallBets();
-  });
+    .schedule("every 15 minutes")
+    .onRun(async (context) => {
+      await settleColorBallBets();
+    });
 
 const settleLotteryBets = async (gameType, minNumber, maxNumber) => {
   try {
@@ -37,11 +37,11 @@ const settleLotteryBets = async (gameType, minNumber, maxNumber) => {
 
     for (const amount of betAmounts) {
       const betsSnapshot = await db
-        .collection("gameBets")
-        .where("settled", "==", false)
-        .where("gameType", "==", gameType)
-        .where("betAmount", "==", amount)
-        .get();
+          .collection("gameBets")
+          .where("settled", "==", false)
+          .where("gameType", "==", gameType)
+          .where("betAmount", "==", amount)
+          .get();
 
       if (betsSnapshot.empty) continue;
 
@@ -51,7 +51,7 @@ const settleLotteryBets = async (gameType, minNumber, maxNumber) => {
 
       betsSnapshot.forEach((betDoc) => {
         const betData = betDoc.data();
-        bets.push({ id: betDoc.id, ...betData });
+        bets.push({id: betDoc.id, ...betData});
         betCount[betData.betNumber] = (betCount[betData.betNumber] || 0) + 1;
         totalBetAmount += betData.betAmount;
       });
@@ -87,7 +87,7 @@ const settleLotteryBets = async (gameType, minNumber, maxNumber) => {
           const updatedWallet = (
             parseInt(userWallet) + totalWinningAmount
           ).toString();
-          await userRef.update({ wallet: updatedWallet });
+          await userRef.update({wallet: updatedWallet});
           await betRef.update({
             rewardAmount: totalWinningAmount,
             settled: true,
@@ -111,8 +111,8 @@ const settleColorBallBets = async () => {
   try {
     const gameBetsRef = db.collection("gameBets");
     const unsettledBetsQuery = gameBetsRef
-      .where("settled", "==", false)
-      .where("gameType", "==", "Color Ball Game");
+        .where("settled", "==", false)
+        .where("gameType", "==", "Color Ball Game");
     const snapshot = await unsettledBetsQuery.get();
     const bets = [];
     const betCount = {};
@@ -120,9 +120,9 @@ const settleColorBallBets = async () => {
     snapshot.forEach((betDoc) => {
       const betData = betDoc.data();
       const combo = `${betData.ballColor}-${betData.betNumber}`;
-      bets.push({ id: betDoc.id, ...betData });
+      bets.push({id: betDoc.id, ...betData});
       if (!betCount[combo]) {
-        betCount[combo] = { count: 0, firstTimestamp: betData.timestamp };
+        betCount[combo] = {count: 0, firstTimestamp: betData.timestamp};
       }
       betCount[combo].count += 1;
       if (betData.timestamp < betCount[combo].firstTimestamp) {
@@ -150,9 +150,9 @@ const settleColorBallBets = async () => {
         winningCombination = null;
       }
 
-      const [winningColor, winningNumber] = winningCombination
-        ? winningCombination.split("-")
-        : [null, null];
+      const [winningColor, winningNumber] = winningCombination ?
+        winningCombination.split("-") :
+        [null, null];
 
       bets.forEach(async (bet) => {
         const betRef = db.collection("gameBets").doc(bet.id);
@@ -166,7 +166,7 @@ const settleColorBallBets = async () => {
           const userDoc = await userRef.get();
           const userWallet = userDoc.data().wallet;
           const updatedWallet = (parseInt(userWallet) + reward).toString();
-          await userRef.update({ wallet: updatedWallet });
+          await userRef.update({wallet: updatedWallet});
           await betRef.update({
             rewardAmount: reward,
             settled: true,
